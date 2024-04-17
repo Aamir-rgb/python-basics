4dc48a4c92ace44221e6631872c44b2f941bf143
Aamir-rgb <aamir.y2k1@gmail.com>
2024-04-17 08:47:40 +0530

    python-basics variable declaration

---
+++ main.py
@@ -0,0 +1,42 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#This Is My First Attempt To Learn Python Programming
+#Version 1 With Double Quotes
+print("Welcome to Azurelib")
+
+#Version 2 With Single Quotes
+print('We started learning python programming in azurelib academy')
+
+100
+
+balance = 100
+
+name = "Azure Lib"
+
+print(balance)
+
+"""
+Python Is Dynamically Typed Language.This is one of the most important feature of the python.
+#Let's focus on the Definition of the Statically Type Language and Dynamically Typed Language.
+Statically Typed Language :
+Those computer languages in which the type of variable is declared at the time of writing that
+variable are referred to as statically typed language.
+e.g. Java,C++,C
+Dynamically Typed Language:
+Those computer languages in which the type of variable is declared at the time of writing that
+variable are referred to as statically typed language.
+e.g. Python,JavaScript
+
+Difference Between Statically Typed And Dynamically Typed
+Statically Typed Language                                  Dynamically Typed Language
+1. We have to write more code.                             We have to write less code.
+2. Implementation logic is somewhat difficult              Implementation logic is easy because while implementing
+   because while implementing we have to take care         we have to take care of each of the variable types.
+   of each of the variable types.
+3. Type checking of the variable occurs at compile time.   Type checking of the variable occurs at runtime.
+4. Most of the time statically typed language produces     Most of the time dynamically typed language produces heavy 
+   lightweight classes                                     classes as compared to statically typed classes.
+5. Difficult to integrate with external libraries          Easy to integrate with external libraries.
+     """
